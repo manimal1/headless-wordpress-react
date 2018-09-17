d7b35609b40c3312327a7e7a4752d9997700653b
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2018-09-17 11:50:32 +0200

    Update registrationReducer.js

--- client/src/reducers/registrationReducer.js
+++ client/src/reducers/registrationReducer.js
@@ -3,14 +3,22 @@ import { userConstants } from '../constants';
 export default function(state = {}, action) {
   switch (action.type) {
     case userConstants.REGISTER_REQUEST:
-      return { registering: true };
+      return {
+        registering: true,
+        registered: false,
+        user: {},
+      };
     case userConstants.REGISTER_SUCCESS:
       return {
         registered: true,
         user: action.user,
       };
     case userConstants.REGISTER_FAILURE:
-      return {};
+      return {
+        registering: false,
+        registered: false,
+        user: {},
+      };
     default:
       return state;
   }
